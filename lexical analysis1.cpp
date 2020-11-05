5e3beee4b2d267ea9c529103d7561071b18395d4
Pretty-man <46771599+Pretty-man@users.noreply.github.com>
2020-11-05 21:26:57 +0800

    Update lexical analysis1.cpp

--- lexical analysis1.cpp
+++ lexical analysis1.cpp
@@ -7,12 +7,14 @@ using namespace std;
 第一类：标识符   letter(letter | digit)*  无穷集
 第二类：常数    (digit)+  无穷集
 第三类：保留字(32)
-auto       break    case     char        const      continue
-default    do       double   else        enum       extern
-float      for      goto     if          int        long
-register   return   short    signed      sizeof     static
-struct     switch   typedef  union       unsigned   void
-volatile    while
+while  for  do  break  continue
+switch  case
+static  return 
+char    const   
+if      else  int   long   unsigned  void
+double  enum    
+struct        
+
 
 第四类：界符  ‘/*’、‘//’、 () { } [ ] " "  '
 第五类：运算符 <、<=、>、>=、=、+、-、*、/、^、
