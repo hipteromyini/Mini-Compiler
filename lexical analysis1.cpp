6928ed88161c695b29fa3fa6c8bd9c25e7e62417
Pretty-man <46771599+Pretty-man@users.noreply.github.com>
2020-11-28 20:03:51 +0800

    Update lexical analysis1.cpp

--- lexical analysis1.cpp
+++ lexical analysis1.cpp
@@ -12,45 +12,48 @@ switch  case
 static  return
 char    const
 if      else  int   long   unsigned  void
-double  enum
+enum
 struct
 
 
 第四类：界符  ‘/*’、‘//’、 () { } [ ] " "  '
 第五类：运算符 <、<=、>、>=、=、+、-、*、/、^、
 
-对所有定义保留字进行编码：
-<int,1>
-<unsigned,2>
-<long,3>
-<float,4>
-<double,5>
-<char,6>
-<string,7>
-<const,8>
-<struct,9>
+对项目支持的保留字进行编码：
 */
+enum {
+    Num=10086 , Fun, Sys, Glo, Loc, Id,Not,
+    Int, Char ,Void, If , Else , For , While , Continue , Return ,
+    Main,Scanf,Printf,
+    Assign,Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt, Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Inc, Dec
+};
+struct identifier {
+    int token;//返回标记
+    int hash;//哈希值
+    char * name;//本身的字符串
+    int clas;
+    int type;//标识符的类型
+    int value;//标识符的值
+};
 struct Word
 {
     string type,statement;
     int val;
-}ans[100005];
-static string wordtype[]={"Int","Unsigned","Long","Float","Double",
-                          "Char","String","Const","Struct"};
-static string keyword[]={"while","for","do","break","continue",
-                         "switch","case","return",
-                         "int","char","double","float","long",
-                         "const","if","else","struct",};//关键字
+}lex[100005];
+//static string keyword[]={"int","char","if","else","for","while","continue","return"};//关键字
 int key_siz;
+const int N=1e5+5;
+char letter[N],token;
+int token_val,sig,hash;
+int Table[N];
+int idmain[N];
+//数值，符号
 static string operaterWord[36] = {
         "+", "-", "*", "/", "<", "<=", ">", ">=", "=", "==",
         "!=", ";", "(", ")", "^", ",", "#", "&",
         "&&", "|", "||", "%", "~", "<<", ">>", "[", "]", "{",
         "}", ".", ":", "!"};//运算符
-const int N=1e5+5;
-char letter[N],token;
-int token_val,sig;
-//数值，符号
+
 
 int len,pos=1,tot;
 //源代码长度，当前短语位置，总短语数
@@ -66,28 +69,33 @@ int iskeyword(string s)
 void next_word()
 {
     token=letter[pos];
-    while(token==' ')//pass all the ' '
+    while(token==' ')//跳过所有空格（' '）
         token=letter[++pos];
-    if(token=='#')//we will not support it
+    if(token=='#')//跳过宏定义
     {
         while(token!=0&&token!='\n')
             token=letter[++pos];
         return;
     }
-    if(token=='-')
+    if(isalpha(token)||token=='_')//字符串
     {
-        if(isdigit(letter[pos+1]))//negative integer
+        string s;
+        while(isalpha(token)||token=='_')
         {
-            sig=-1;
-            next_word();
+            s+=token;
+            token=letter[++pos];
         }
-        else //operater -
+        /*
+         * 判断是否是保留字
+         * int id=iskeyword(s);
+        if(id) lex[++tot]={keyword[id-1]};
+        else
         {
-            ans[++tot]={"-"};
-        }
+            lex[++tot]={"String"};
+        }*/
         return;
     }
-    if(isdigit(token))// positive integer
+    if(isdigit(token))// 正数
     {
         token_val=0;
         while(isdigit(token))
@@ -97,31 +105,154 @@ void next_word()
         }
         token_val*=sig;
         sig=1;
-        ans[++tot]={"Int","",token_val};
+        token=Num;
         return;
     }
-    if(isalpha(token)||token=='_')
+    if(token=='+')
     {
-        string s;
-        while(isalpha(token)||token=='_')
+        if(letter[pos+1]=='+')
         {
-            s+=token;
-            token=letter[++pos];
+            pos++;
+            token=Inc;
         }
-        int id=iskeyword(s);
-        if(id) ans[++tot]={keyword[id-1]};
-        else 
+        else // - 运算符
         {
-            ans[++tot]={"String"};
+            token=Add;
         }
         return;
     }
-    
+    if(token=='-')
+    {
+        if(letter[pos+1]=='-')
+        {
+            pos++;
+            token=Dec;
+        }
+        if(isdigit(letter[pos+1]))//负数
+        {
+            sig=-1;
+            next_word();
+        }
+        else // - 运算符
+        {
+            token=Sub;
+        }
+        return;
+    }
+    if(token=='*')
+    {
+        token=Mul;
+        return;
+    }
+    if(token=='/')
+    {
+        if(letter[pos+1]=='/')
+        {
+            // 单行注释
+            token=letter[++pos];
+            while(token!=0&&token!='\n')
+            {
+                token=letter[++pos];
+            }
+            return;
+        }
+        else
+        {
+            // 除运算符
+            token=Div;
+            return;
+        }
+    }
+    if(token=='=')
+    {
+        if(letter[pos+1]=='=')
+        {
+            pos++;
+            token=Eq;//==
+        }
+        else token=Assign;//=
+        return;
+    }
+    if(token=='!')
+    {
+        if(letter[pos+1]=='=')
+        {
+            pos++;
+            token=Ne;//不等于
+        }
+        else
+        {
+            token=Not;//取反
+        }
+        return;
+    }
+    if(token=='<')
+    {
+        if(letter[pos+1]=='<')
+        {
+            pos++;
+            token=Shl;//左移
+        }
+        else if(letter[pos+1]=='=')
+        {
+            pos++;
+            token=Le;//<=
+        }
+        else token=Lt;//<
+        return;
+    }
+    if(token=='>')
+    {
+        if(letter[pos+1]=='>')
+        {
+            pos++;
+            token=Shr;//右移
+        }
+        else if(letter[pos+1]=='=')
+        {
+            pos++;
+            token=Ge;//>=
+        }
+        else token=Gt;//>
+        return;
+    }
+    if(token=='|')
+    {
+        if(letter[pos+1]=='|')
+        {
+            pos++;
+            token=Lor;//||
+        }
+        else token=Or;//|运算
+        return;
+    }
+    if(token=='&')
+    {
+        if(letter[pos+1]=='&')
+        {
+            pos++;
+            token=Lan;//&&
+        }
+        else token=And;//&
+        return;
+    }
+    if(token=='^')
+    {
+        token=Xor;
+        return;
+    }
+
+}
+void init()
+{
+    string s="int char void if else for  while  continue  return "
+             " main scanf printf";
 }
 int main(int argc,char** argv)
 {
     char c;
-    freopen("in.txt","r",stdin);
+//    freopen("in.txt","r",stdin);
+    init();
     while(cin>>c)
         letter[++len]=c;
     while(pos<=len)
